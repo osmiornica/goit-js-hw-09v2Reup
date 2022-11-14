ea24d1f0c530529228a1b6bf8fd5c4789019aef2
osmiornica <95519314+osmiornica@users.noreply.github.com>
2022-11-14 22:08:18 +0000

    Deploying to gh-pages from @ osmiornica/goit-js-hw-09v2Reup@ba0058a8632897eeb2e2cbc1ee2f0f000bce67b8 🚀

---
+++ 01-color-switcher.94acf48d.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),o=document.querySelector("body");e.addEventListener("click",(function(){t=setInterval((function(){o.style.backgroundColor="#".concat(Math.floor(16512141*Math.random()).toString(16))}),1e3),e.setAttribute("disabled","disabled")})),n.addEventListener("click",(function(){e.hasAttribute("disabled")&&(clearInterval(t),e.removeAttribute("disabled"))}))}();
+//# sourceMappingURL=01-color-switcher.94acf48d.js.map
